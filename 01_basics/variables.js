7a0594519318cbfd2f76ff4f08d99852d66c79d5
PrachiPatil-droid <24je0149@iitism.ac.in>
2025-12-07 12:42:31 +0000

    datatypes

---
+++ 01_basics/variables.js
@@ -0,0 +1,19 @@
+"use strict";// treat all js code as newer version
+
+// alert(3+3) //we are using node js naot browser
+/*//data types:
+//number 2 to power 53
+//bigint
+//string ""
+//boolean
+//null standalone value
+//undefined
+//symbol
+
+//object
+*/
+let age=3
+console.log(typeof age);
+
+console.log(typeof null)
+console.log(typeof undefined)
